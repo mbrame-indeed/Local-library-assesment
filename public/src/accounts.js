a6266fced1977f34878295ed2b7d1293605ffc5e
Michael Brame <mbrame@indeed.com>
2023-10-29 20:53:10 -0500

    Commiting completed local library assessment project

---
+++ public/src/accounts.js
@@ -0,0 +1,45 @@
+function findAccountById(accounts, id) {
+  const findByIdResult = accounts.find((account) => account.id === id);
+  return findByIdResult;
+}
+
+function sortAccountsByLastName(accounts) {
+  const sortByLastNameResult = accounts.sort((accountA, accountB) => (accountA.name.last < accountB.name.last) ? -1 : 1);
+  return sortByLastNameResult;
+}
+
+function getAccountFullNames(accounts) {
+  const fullName = accounts.map((account) => `${account.name.first} ${account.name.last}`);
+  return fullName;
+}
+
+// NOTE: YOU DON'T HAVE TO EDIT THE FUNCTIONS BELOW
+function getTotalNumberOfBorrows(account, books) {
+  return books.reduce((acc, book) => {
+    const count = book.borrows.reduce((borrowAcc, borrow) => {
+      return borrow.id === account.id ? borrowAcc + 1 : borrowAcc;
+    }, 0);
+
+    return acc + count;
+  }, 0);
+}
+
+function getBooksPossessedByAccount(account, books, authors) {
+  return books
+    .filter((book) => {
+      const recent = book.borrows[0];
+      return !recent.returned && recent.id === account.id;
+    })
+    .map((book) => {
+      const author = authors.find((author) => author.id === book.authorId);
+      return { ...book, author };
+    });
+}
+
+module.exports = {
+  findAccountById,
+  sortAccountsByLastName,
+  getAccountFullNames,
+  getTotalNumberOfBorrows,
+  getBooksPossessedByAccount,
+};
---
+++ public/src/books.js
@@ -0,0 +1,47 @@
+function findAuthorById(authors, id) {
+  const authorById = authors.find((author) => author.id === id);
+  return authorById;
+}
+
+function findBookById(books, id) {
+  const bookById = books.find((book) => book.id === id);
+  return bookById;}
+
+// NOTE: YOU DON'T HAVE TO EDIT THE FUNCTIONS BELOW
+function partitionBooksByBorrowedStatus(books) {
+  return books.reduce(
+    (acc, book) => {
+      const [borrowed, returned] = acc;
+      const recent = book.borrows[0];
+      if (recent.returned) {
+        returned.push(book);
+      } else {
+        borrowed.push(book);
+      }
+
+      return acc;
+    },
+    [[], []]
+  );
+}
+
+function getBorrowersForBook(book, accounts) {
+  const accountsById = accounts.reduce((acc, account) => {
+    acc[account.id] = account;
+    return acc;
+  }, {});
+
+  return book.borrows
+    .map(({ id, returned }) => ({
+      ...accountsById[id],
+      returned,
+    }))
+    .slice(0, 10);
+}
+
+module.exports = {
+  findAuthorById,
+  findBookById,
+  partitionBooksByBorrowedStatus,
+  getBorrowersForBook,
+};
